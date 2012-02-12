f517bf92ee5e57a4dc8c6eb4cec47e754bc4e381
Roly Fentanes <roly426@gmail.com>
2012-02-11 21:11:37 -0700

    first commit

---
+++ test/workerworker-test.js
@@ -0,0 +1,66 @@
+var hub = require('..')
+  , cluster = require('cluster')
+  , assert = require('assert')
+  , WORKERS = 2
+
+
+if (cluster.isMaster) {
+  workers = [];
+  for (var i = 0; i < WORKERS; i++) {
+    workers.push(cluster.fork());
+  }
+
+  var n = WORKERS;
+  hub.on('imready', function() {
+    if (--n === 0) hub.emit('allready');
+  });
+
+  describe('Master', function() {
+
+    it('Waits for workers to exit', function(done) {
+      var n = WORKERS;
+      function exit() {
+        if (--n === 0) done();
+      }
+
+      cluster.on('death', exit);
+    });
+
+  });
+
+} else {
+
+  describe('Worker', function() {
+
+    describe('Emit message to other worker', function() {
+      it('Respond when all workers are listening', function(done) {
+        hub.on('fromworker', done);
+
+        hub.on('allready', function() {
+          hub.emitRemote('fromworker');
+        });
+        hub.emit('imready');
+      });
+    });
+
+    describe('Calls hub method', function() {
+
+      it('Data should be shared amongst workers', function(done) {
+        var n = 0;
+        hub.on('incr work', function() {
+          if (++n === WORKERS) {
+            done();
+          }
+        });
+
+        hub.ready(function() {
+          setTimeout(function() {
+          hub.incr('work');
+          }, 100);
+        });
+      });
+      
+    });
+  });
+
+}
