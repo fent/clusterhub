f66d0e0aeff328ad473a101cb9d160669d8b3693
fent <933490+fent@users.noreply.github.com>
2018-09-15 16:43:10 -0400

    test: make sure coverage results are combined

--- test/fork.js
+++ test/fork.js
@@ -3,14 +3,15 @@ const path    = require('path');
 
 
 // Forks a new worker with istanbul test coverage.
-module.exports = (file) => {
-  file = path.join(__dirname, file);
+module.exports = (filename) => {
+  const filepath = path.join(__dirname, filename);
   cluster.setupMaster(process.env.running_under_istanbul ? {
     exec: './node_modules/.bin/istanbul',
     args: [
-      'cover', '--report', 'none', /*'--dir', `./coverage/each/${file}`,*/
-      '--print', 'none', '--include-pid', file, '--'
+      'cover', '--report', 'json',
+      '--dir', `./coverage/each/workers/${filename}`,
+      '--print', 'none', '--include-pid', filepath, '--'
     ].concat(process.argv.slice(2))
-  } : { exec: file });
+  } : { exec: filepath });
   return cluster.fork();
 };
--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,6 +1,4 @@
-find test -name "*-test.js" | \
-  grep -o -E "\/([^\.]+)" | \
-  xargs -I {} -n 1 sh -c 'export f="{}"; \
-  istanbul cover --report json --dir coverage/each${f} --print none node_modules/.bin/_mocha -- --exit test${f}.js'
+istanbul cover --report json --dir coverage/each/master --print none \
+  ./node_modules/.bin/_mocha -- -t 4000 test/*-test.js
 istanbul report --root coverage/each --dir coverage/full lcov
-istanbul report --root coverage/each text-summary
+istanbul report --root coverage text-summary
