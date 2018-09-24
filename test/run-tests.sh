4c50e382d6959b7c9a325bafe554298292378997
fent <933490+fent@users.noreply.github.com>
2018-09-23 22:51:29 -0400

    test: fix script for running tests

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,3 +1,4 @@
+rm -rf coverage/
 istanbul cover --report json --dir coverage/each/master --print none \
   ./node_modules/.bin/_mocha -- -t 4000 test/*-test.js
 istanbul report --root coverage/each --dir coverage/full lcov
